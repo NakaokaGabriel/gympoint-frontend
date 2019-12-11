01b905d2df24a42e842c39ff55fc7eba7182e56b
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-11 18:46:00 -0300

    Gympoint academy

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', {
+      extensions: ['.jsx', '.js']
+    }],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', {
+      argsIgnorePattern: '^_'
+    }],
+    'react/no-expression-per-line': 'off',
+    'global-require': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+  },
+};

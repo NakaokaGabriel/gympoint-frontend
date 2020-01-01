b92e9b83bbac56606ed80900a3815e5dad3e83b6
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-31 21:55:10 -0300

    Added functionality to create students

---
+++ src/store/modules/student/reducer.js
@@ -0,0 +1,25 @@
+import produce from 'immer';
+
+const INITIAL_STATE = {
+  loading: false,
+};
+
+export default function student(state = INITIAL_STATE, action) {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@student/REGISTER_REQUEST': {
+        draft.loading = true;
+        break;
+      }
+      case '@student/REGISTER_SUCCESS': {
+        draft.loading = false;
+        break;
+      }
+      case '@student/FAILURE': {
+        draft.loading = false;
+        break;
+      }
+      default:
+    }
+  });
+}

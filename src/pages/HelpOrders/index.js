d18a04c239ff7a96b944c3718da98d4b64e97ed1
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-03-10 23:00:53 -0300

    feature: listing orders

--- src/pages/HelpOrders/index.js
+++ src/pages/HelpOrders/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '~/services/api';
 
 import { Header } from './styles';
 import Content from '~/components/Content';
 import Table from '~/components/Table';
 
 export default function HelpOrders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const response = await api.get(`help-orders`);
+
+      setOrders(response.data);
+    }
+
+    loadOrders();
+  }, []);
+
   return (
     <>
       <Header>
@@ -18,12 +32,14 @@ export default function HelpOrders() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Lennert Nijenbijvank</td>
-              <td>
-                <button type="button">responder</button>
-              </td>
-            </tr>
+            {orders.map(order => (
+              <tr key={order.id}>
+                <td>{order.student.name}</td>
+                <td>
+                  <button type="button">responder</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Content>

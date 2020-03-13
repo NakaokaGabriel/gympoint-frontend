ceeb20e7650194aaa2a762563ef87725ea9f1ac5
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-03-12 22:54:39 -0300

    feature: modal action

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { Background, Content } from './styles';
 
-export default function Modal({ children, modal }) {
+export default function Modal({ children, modal, closeModal }) {
+  const box = useRef();
+
+  useLayoutEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'hidden';
+    };
+  }, []);
+
+  function handleWindowClick(event) {
+    if (!box.current.contains(event.target)) {
+      closeModal();
+    }
+  }
+
   return (
-    <Background modal={modal}>
-      <Content>{children}</Content>
+    <Background modal={modal} onClick={handleWindowClick}>
+      <Content ref={box}>{children}</Content>
     </Background>
   );
 }
@@ -14,4 +30,5 @@ export default function Modal({ children, modal }) {
 Modal.propTypes = {
   children: PropTypes.element.isRequired,
   modal: PropTypes.bool.isRequired,
+  closeModal: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]).isRequired,
 };
